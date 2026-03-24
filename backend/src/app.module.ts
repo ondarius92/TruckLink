b5738833d32f762ed0713fbc8d26b88f70cc8137
ondarius92 <on.darius92@gmail.com>
2026-03-25 00:03:45 +0200

    Create app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,57 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD, APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { appConfig, jwtConfig, awsConfig } from './config';
+import { PrismaService } from './database/prisma.service';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { MatchingModule } from './modules/matching/matching.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { DisputesModule } from './modules/disputes/disputes.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, jwtConfig, awsConfig],
+    }),
+    AuthModule,
+    UsersModule,
+    OrdersModule,
+    MatchingModule,
+    PaymentsModule,
+    VehiclesModule,
+    ReviewsModule,
+    DisputesModule,
+    NotificationsModule,
+  ],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
+  exports: [PrismaService],
+})
+export class AppModule {}
